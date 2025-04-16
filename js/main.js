b38be05a22ca77d27a5a6f55f029a724dbca3358
Seotrafter <ts@traffter.com>
2025-04-16 18:02:57 +0300

    Add files via upload
    initial commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const cookieBanner = document.getElementById("cookie-banner");
+    const acceptBtn = document.getElementById("accept-cookies");
+    const declineBtn = document.getElementById("decline-cookies");
+
+    if (cookieBanner && !localStorage.getItem("cookiesAccepted") && !localStorage.getItem("cookiesDeclined")) {
+        cookieBanner.style.display = "flex";
+    }
+
+    if (acceptBtn) {
+        acceptBtn.addEventListener("click", function () {
+            localStorage.setItem("cookiesAccepted", "true");
+            if (cookieBanner) cookieBanner.style.display = "none";
+        });
+    }
+
+    if (declineBtn) {
+        declineBtn.addEventListener("click", function () {
+            localStorage.setItem("cookiesDeclined", "true");
+            if (cookieBanner) cookieBanner.style.display = "none";
+        });
+    }
+
+});
+
